25d373ddb40c9517efbeab249b5c223b9d175486
atlhpfun <atlhpfun@gmail.com>
2024-06-27 13:25:39 -0400

    test

--- receipts-app/src/components/AuthButton.js
+++ receipts-app/src/components/AuthButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { MissingRefreshTokenError } from '@auth0/auth0-react';
 
 const AuthButton = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
